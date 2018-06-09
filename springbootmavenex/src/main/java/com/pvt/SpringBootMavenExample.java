3d691d34087ebcb8f53b0a46537c63b79b4878d1
Polisetty Venkatesh <polisetty.venkatesh@gmail.com>
2018-06-09 08:38:27 +0530

    updated project name

--- springbootmavenex/src/main/java/com/pvt/SpringBootMavenExample.java
+++ springbootmavenex/src/main/java/com/pvt/SpringBootMavenExample.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootExample {
+public class SpringBootMavenExample {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExample.class, args);
+		SpringApplication.run(SpringBootMavenExample.class, args);
 		System.out.println("abc");
 	}
 
