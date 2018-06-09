1ef779b4bf9504efff9d45d2783503a328ec2085
Polisetty Venkatesh <polisetty.venkatesh@gmail.com>
2018-06-09 07:28:54 +0530

    Initial Changes

---
+++ src/main/java/com/pvt/SpringBootExample.java
@@ -0,0 +1,14 @@
+package com.pvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExample {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExample.class, args);
+		System.out.println("abc");
+	}
+
+}
