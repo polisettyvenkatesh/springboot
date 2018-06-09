879bfde3477c263951288fc8387d90f5081678a2
Polisetty Venkatesh <polisetty.venkatesh@gmail.com>
2018-06-09 09:26:09 +0530

    Hello page example

---
+++ springbootHelloPageEx/src/main/java/com/pvt/hello/SpringbootHelloPageExApplication.java
@@ -0,0 +1,12 @@
+package com.pvt.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHelloPageExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHelloPageExApplication.class, args);
+	}
+}
