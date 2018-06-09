a9fa05af368a4a52109ec6559431acfa8b542f98
Polisetty Venkatesh <polisetty.venkatesh@gmail.com>
2018-06-09 09:07:06 +0530

    Starter example project

---
+++ springbootstarterexample/src/main/java/com/example/demo/SpringbootstarterexampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstarterexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstarterexampleApplication.class, args);
+	}
+}
