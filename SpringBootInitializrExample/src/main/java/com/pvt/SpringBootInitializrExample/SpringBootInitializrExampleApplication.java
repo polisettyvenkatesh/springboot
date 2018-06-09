4331303ca4b351db16a4fbd0982ede1b4e15b742
Polisetty Venkatesh <polisetty.venkatesh@gmail.com>
2018-06-09 08:46:52 +0530

    Initializr project added

---
+++ SpringBootInitializrExample/src/main/java/com/pvt/SpringBootInitializrExample/SpringBootInitializrExampleApplication.java
@@ -0,0 +1,12 @@
+package com.pvt.SpringBootInitializrExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitializrExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootInitializrExampleApplication.class, args);
+	}
+}
